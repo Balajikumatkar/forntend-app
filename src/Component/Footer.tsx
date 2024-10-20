f47093741c767ff1b10cd9c209238b42e7dbd74e
Balajikumatkar <51821447+Balajikumatkar@users.noreply.github.com>
2024-10-20 22:03:30 +0530

    Add files via upload

---
+++ src/Component/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="Footer">
+      <p>&copy; 2023 Fertilizer and Pesticides Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/config/config.ts
@@ -0,0 +1,5 @@
+const config = {
+    apiBaseUrl: 'https://localhost:44347/api',
+};
+
+export default config;
